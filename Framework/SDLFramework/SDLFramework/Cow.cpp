529a1fe261fc80e7fce7dd0dc18d04a4828dc109
Sagar Gangabisoensingh <sagartje1993@gmail.com>
2015-05-01 16:40:17 +0200

    fixed cow

--- Framework/SDLFramework/SDLFramework/Cow.cpp
+++ Framework/SDLFramework/SDLFramework/Cow.cpp
@@ -2,10 +2,17 @@
 
 Cow::Cow()
 {
+	init();
+}
+
+void Cow::init()
+{
+	application = FWApplication::GetInstance();
 	cowTexture = application->LoadTexture("cow-1.png");
 }
 
-
 Cow::~Cow()
 {
+	application = nullptr;
+	SDL_DestroyTexture(cowTexture);
 }
--- Framework/SDLFramework/SDLFramework/Cow.h
+++ Framework/SDLFramework/SDLFramework/Cow.h
@@ -3,6 +3,7 @@
 #include "FWApplication.h"
 #include <SDL_events.h>
 #include "SDL_timer.h"
+#include <SDL.h>
 #include <time.h>
 #include "Graph.h"
 class Cow
@@ -12,6 +13,7 @@ public:
 	SDL_Texture* cowTexture;
 
 	Cow();
+	void init();
 	virtual ~Cow();
 };
 
